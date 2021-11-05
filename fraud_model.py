d09cc61601102a721ce2c9f79a8abfe891236c07
ahagler <42753663+ahagler@users.noreply.github.com>
2021-11-04 21:50:18 -0400

    add serialization

--- fraud_model.py
+++ fraud_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import xgboost as xgb
+import pickle
 
 best_features = ['isFraud', 'card2', 'addr1', 'C4', 'C10', 'V87', 'card5', 'D4', 'C11', 'TransactionAmt', 'D1', 'C8', 'C6', 'D2', 'C13', 'C14', 'TransactionDT', 'D15', 'C1', 'C2', 'card1']
 def clean_data(data):
@@ -26,4 +27,4 @@ def build_model(X, y):
 	    random_state=2
 	)
 	model.fit(X.values, y.values, eval_metric="auc")
-	return model
+	return pickle.dumps(model)
